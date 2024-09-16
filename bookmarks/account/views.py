1b6ae00eb04a771de67422cfcb99072ce09473cf
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-09-17 01:53:08 +0300

    password-change && password-change/done

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .forms import LoginForm
-from django.contrib.auth import authenticate,login
+# from django.http import HttpResponse
+# from .forms import LoginForm
+# from django.contrib.auth import authenticate,login
 from django.contrib.auth.decorators import login_required
 
 # # Create your views here.
