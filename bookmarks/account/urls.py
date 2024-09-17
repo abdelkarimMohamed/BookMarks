d45e4d3a0d1210e594041b8ac13b74713b1ed231
AbdelkarimMohamedAli <abdelkarimmohamedali81@gmail.com>
2024-09-17 05:07:55 +0300

    User Registration

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -16,5 +16,6 @@ urlpatterns = [
 
     path('', include('django.contrib.auth.urls')),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
 
 ]
